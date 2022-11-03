205ffbb5f59be0a6adebc588b49074ea242b399f
tar0324 <63778614+tar0324@users.noreply.github.com>
2022-11-03 12:23:35 +0900

    Add files via upload

---
+++ pro19/src/main/java/com/spring/ex04/LazyTest.java
@@ -0,0 +1,16 @@
+package com.spring.ex04;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class LazyTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext context = new FileSystemXmlApplicationContext("lazy.xml");
+		System.out.println("SecondBean 얻기");
+		context.getBean("secondBean");
+
+	}
+
+}
